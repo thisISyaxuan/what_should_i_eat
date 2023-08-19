d609b998b5dfe32d3023d2ea73b450a88369f637
eduion <36690580+eduion@users.noreply.github.com>
2023-08-20 00:25:45 +0800

    Update App.js

--- 前端/App.js
+++ 前端/App.js
@@ -1,28 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import AuthStack from "./routes/AuthStack";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from "react";
-export default function App() {
-  const [userToken, setUserToken] = useState(null);
-  useEffect(() => {
-    // 在 app 啟動時檢查是否有 token
-    checkTokenAndAutoLogin();
-  }, []);
 
-  const checkTokenAndAutoLogin = async () => {
-    try {
-      const token = await AsyncStorage.getItem("userToken");
-      if (token) {
-        setUserToken(token);//先存token的值
-        // 導航到主頁面，因為有可用的 token
-        navigation.navigate("ButtomTabStack"); // 假設這個堆疊的名稱是 "ButtomTabStack"
-      }
-    } catch (error) {
-      console.error("Error checking token:", error);
-    }
-  };
+export default function App({navigation}) {
+  const [userToken, setUserToken] = useState(null);
+  
 
   return (
     <NavigationContainer>
