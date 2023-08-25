2d612a8522f44cee1063e84713344ab12e4adb0d
109213054 <115973449+109213054@users.noreply.github.com>
2023-08-25 21:04:38 +0800

    Update Wallet.js

--- 前端/screens/Wallet/Wallet.js
+++ 前端/screens/Wallet/Wallet.js
@@ -1,33 +1,65 @@
-import {StyleSheet, Text, View,TouchableOpacity,Modal,SafeAreaView} from 'react-native';
-import { globalStyles } from '../../styles/global';
-import React, {useState} from 'react';
-import {Calendar, LocaleConfig} from 'react-native-calendars';
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FlatList } from 'react-native-gesture-handler';
+
 export default function Wallet() {
-  const [selected, setSelected] = useState('');
-  const [money, setMoney] = useState(null);
-  const [rating, setRating] = useState(null);
+  
+  const resd = {
+    "data": [
+      {"cid": 1, "resname": "蘇嬤嬤湯圓", "which_meal": 0, "price": 70, "rating": 4.1},
+      {"cid": 2, "resname": "大仁湯包湯餃", "which_meal": 1, "price": 80, "rating": 5.0},
+      {"cid": 3, "resname": "麥當勞", "which_meal": 2, "price": 90, "rating": 4.5}
+    ],
+    "total": {"total": 240}
+  };
+
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split('T')[0];
+  const [selected, setSelected] = useState(formattedDate);//一開始為當下時間
+  const [dateData, setDateData] = useState([]);//依日期對應的記帳資訊
+  const [total, setTotal] = useState(0);//總金額
+
+  useEffect(() => {
+    fetchDateData(selected); // 初始化時取得當天數據
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.costdetail}>
+      <View style={styles.circle}>
+        <Text style={styles.textty}>{item.which_meal}</Text>
+      </View>
+      <View style={styles.square}>
+        <Text style={[styles.textsquare, { fontWeight: 'bold' }]}>{item.resname}</Text>
+        <Text>{item.rating}顆星</Text>
+      </View>
+      <View style={styles.money}>
+        <Text style={styles.textMoney}>$ {item.price}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   const fetchDateData = async (date) => {
     try {
       const userToken = await AsyncStorage.getItem('userToken');
       if (userToken) {
         const data = {
-          date: date
+          date: date,
         };
         const response = await fetch('http://192.168.0.2:8000/api/getCostDetail/', {
           method: 'POST',
           headers: {
-            'Authorization': `Token ${userToken}`,
-            'Content-Type': 'application/json'
+            Authorization: `Token ${userToken}`,
+            'Content-Type': 'application/json',
           },
-          body: JSON.stringify(data)
+          body: JSON.stringify(data),
         });
         const responseData = await response.json();
-        setMoney(responseData.money);
-        setRating(responseData.rating);
-      }else{
-        console.log('無token')
+        setDateData(responseData.data);
+        setTotal(responseData.total.total);
+      } else {
+        console.log('找不到token');
       }
     } catch (error) {
       console.error('Error fetching date data:', error);
@@ -36,26 +68,24 @@ export default function Wallet() {
 
   return (
     <SafeAreaView>
-    <Calendar
-      onDayPress={day => {
-        setSelected(day.dateString);
-        fetchDateData(day.dateString);
-      }}
-      markedDates={{
-        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-      }}
-    />
-
-    {/*
-    <View style={styles.dateDataContainer}>
-        <Text>Money: {money !== null ? money : 'N/A'}</Text>
-        <Text>Rating: {rating !== null ? rating : 'N/A'}</Text>
-      </View>
-    */}
-
+      <Calendar
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+          fetchDateData(day.dateString);
+        }}
+        markedDates={{
+          [selected]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' },
+        }}
+      />
+      <FlatList
+        data={dateData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.cid.toString()}
+      />
+      <Text>總支出: ${total}</Text>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -63,15 +93,48 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5F5F5',
-    
-  },
-  text:{
-    fontSize:50,
   },
   dateDataContainer: {
     margin: 20,
     padding: 10,
     backgroundColor: '#f0f0f0',
     borderRadius: 8,
+  },
+  costdetail:{
+    backgroundColor:'white',
+    flexDirection:'row',
+    padding:10,
+  },
+  circle:{
+    width:40,
+    height:40,
+    alignContent:'center',
+    justifyContent:'center',
+    backgroundColor:'green',
+    borderRadius:'50%',
+    padding:10,
+  },
+  square:{
+    backgroundColor:'white',
+    flex:6,
+    padding:10,
+  },
+  money:{
+    padding:10,
+    flex:2,
+    justifyContent: 'center'
+    //alignContent:'flex-end',
+  },
+  textty:{
+    fontSize:20,
+    color:'white',
+  },
+  textsquare:{
+    fontSize:16,
+  },
+  textMoney:{
+    textAlign: 'right',
+    fontSize:18,
+    fontWeight:'bold',
   },
 });
