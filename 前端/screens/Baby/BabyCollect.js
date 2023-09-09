75fea00288b92ff5e06f34a17840e07d2f3d2095
Han899 <s259822431@gmail.com>
2023-09-09 15:47:08 +0800

    精靈 更1

--- 前端/screens/Baby/BabyCollect.js
+++ 前端/screens/Baby/BabyCollect.js
@@ -3,37 +3,48 @@ import { View, StyleSheet, Image, FlatList, Text, TouchableOpacity } from 'react
 import { useNavigation } from "@react-navigation/native";
 import { images } from '../../data/babyImage';
 import { baby_DATA } from '../../data/baby';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const BabyCollect = () => { 
     const navigation = useNavigation();
-    const [ownedBabies, setOwnedBabies] = useState([]); // 定義一個狀態來儲存使用者已擁有的精靈id
+    const [ownedBabies, setOwnedBabies] = useState([]); // 使用useState來保存用戶已擁有的精靈id列表
 
-    // 使用useEffect進行組件初始化時的API請求
     useEffect(() => {
-        // API位址(這裡需要替換為真實的位址)
-        fetch('https://yourapi.com/getUserBabies', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                token: 'your_token_here', // 替換成實際使用的token
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            setOwnedBabies(data.ownedBabies); // 更新已擁有精靈的狀態
-        })
-        .catch((error) => {
-            console.error('獲取數據出錯:', error);
-        });
+        const fetchOwnedBabies = async () => {
+            try {
+                const token = await AsyncStorage.getItem('userToken'); // 從AsyncStorage中取得token
+                if (token) {
+                    // 使用token發送請求到後端取得使用者數據(使用實際API連結)
+                    fetch('https:', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}` // 使用Bearer token方式進行認證，根據您的後端來調整
+                        },
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        setOwnedBabies(data.ownedBabies); // 從後端取得的已擁有精靈id列表
+                    })
+                    .catch((error) => {
+                        console.error('獲取數據出錯:', error);
+                    });
+                } else {
+                    console.log('未能取得token');
+                }
+            } catch (error) {
+                console.error('Error fetching token:', error);
+            }
+        };
+
+        fetchOwnedBabies(); // 執行上面的函數
     }, []);
 
     const renderItem = ({ item, index }) => (
         <View style={styles.circle}>
             <TouchableOpacity style={styles.image}>
                 <Image style={styles.pic} source={item} />
-                {/* 如果該用戶未擁有這個精靈，則添加灰色蒙板 */}
+                {/* 如果該精靈不在已擁有列表中，則加上灰色蒙板 */}
                 {!ownedBabies.includes(baby_DATA[index].id) && 
                     <View style={[StyleSheet.absoluteFill, {backgroundColor: 'rgba(0,0,0,0.7)'}]} />}
             </TouchableOpacity>
@@ -58,6 +69,7 @@ const BabyCollect = () => {
     );
 };
 
+// 樣式部分維持不變
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -68,29 +80,33 @@ const styles = StyleSheet.create({
     listContainer: {
         flexGrow: 1,
         justifyContent: 'space-between',
-        padding:5,
+        padding: 5,
+    },
+    itemContainer: {
+        marginBottom: 10,
+        alignItems: 'center', 
     },
     circle: {
         flexDirection: 'column',
         width: '33%',
-        height:140,
+        height: 140,
         alignItems: 'center', 
     },
     image: {
-        borderWidth:1,
-        borderColor:'#f6d58a',
+        borderWidth: 1,
+        borderColor: '#f6d58a',
         width: '90%',
         aspectRatio: 1,
-        borderRadius:60,
+        borderRadius: 60,
         alignItems: 'center', 
-        justifyContent:'center',
-        padding:5,
+        justifyContent: 'center',
+        padding: 5,
     },
     money: {
         marginTop: 'auto',
         flexDirection: 'row',
-        padding:5,
-        width:'60%',
+        padding: 5,
+        width: '60%',
         alignItems: 'center', 
         justifyContent: 'center',
     },
@@ -102,6 +118,11 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         aspectRatio: 1,
+    },
+    imageText: {
+        marginTop: 5,
+        fontSize: 12,
+        color: 'gray',
     },
 });
 
