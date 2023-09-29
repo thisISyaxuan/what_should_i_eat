ccb990c921d37d8fa0758bfd17c0f9f35c8499b0
109213054 <115973449+109213054@users.noreply.github.com>
2023-09-29 17:54:20 +0800

    Update Home.js

--- 前端/screens/Home/Home.js
+++ 前端/screens/Home/Home.js
@@ -4,7 +4,6 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import EventList from '../../component/event-list';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Location from 'expo-location';
-import {Back_Test_Data} from '../../data/backtestdata';
 
 
 export default function Home() {
@@ -13,14 +12,13 @@ export default function Home() {
     const data = route.params && route.params.data ? route.params.data : { success: 2 };
     const [lastSentPos, setLastSentPos] = useState([0,0]);
     const [dataLoaded,setDataLoaded] = useState(false);//追蹤資料有沒有都抓取成功了
+    const [datacontent,setDatacontent] = useState();//傳給EventList的資料
 
     useEffect(() => {
         if (data.success != 2){//從篩選條件返回的參數
-            //console.log("我是Home,剛剛在篩選條件頁面得到參數:",data);// {"DistanceSort": false, "LabelFilter": "麵食", "MealFilter": -1, "RatingSort": true, "TimeFilter": true, "userPos": [23.01, 120.01]}}
+            setDatacontent(data.success)
             setDataLoaded(true);
         }else{
-            //console.log("我是Home,從其他頁面過來的")
-            //從其他頁面進來的
             checkLocationPermission();
             fetchRestaurants(); // 執行上面的函數
     }
@@ -58,20 +56,17 @@ export default function Home() {
                     });
                     if (response.ok) {
                         const responseData = await response.json();//把後端回傳的資料放在responsData
-                        console.log(responseData);//原本是寫responseData.success，但我覺得沒success，所以拿掉了
-                        data = responseData;//我覺得這裡可能有錯
-                        //如果data不能被改寫，我要怎麼把全域變數設為回傳資料的值，然後傳給EventList勒...
+                        console.log(responseData.success);
+                        setDatacontent(responseData.success);
                         setDataLoaded(true);// 在這裡設定資料載入完成的狀態
                         //const data = responseData.success;
                     } else {
                         console.error('後端回傳發生錯誤嗚嗚嗚', response.status);
                     }
                 }else{
-                    // 如果經度和緯度變化不超過0.001，則不發送請求
-                    setDataLoaded(true);
+                    setDataLoaded(true); // 如果經度和緯度變化不超過0.001，則不發送請求
                 }
             } else {
-                //console.log('Home抓不到token');
                 const location = await Location.getCurrentPositionAsync({});//抓經緯
                 const newCoords = [location.coords.latitude, location.coords.longitude];
                 if (Math.abs(newCoords[0] - lastSentPos[0]) > 0.001 ||Math.abs(newCoords[1] - lastSentPos[1]) > 0.001){//判斷經緯有沒有超過0.001
@@ -85,20 +80,21 @@ export default function Home() {
                         RatingSort: false
                       };
                       console.log("這是我要傳給後端的資料:", requestdata);
+                      console.log("成功");
                 }else{
-                    //console.log('經度和緯度變化不超過0.001，不發送請求');
-                    setDataLoaded(true);
+                    setDataLoaded(true);//console.log('經度和緯度變化不超過0.001，不發送請求');
                 }
             }
         } catch (error) {
             console.error('Home Error sending request:', error);
+
         }
     };
 
     return (
         <View style={styles.container}>
             {/* 將餐廳資料傳給EventList組件 */}
-            {dataLoaded ? <EventList data={data} /> : null}
+            {dataLoaded ? <EventList data={datacontent} /> : null}
         </View>
     );
 }
