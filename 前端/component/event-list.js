7169691402b6e5a18bd60c3b9672f1d42be43ad1
109213054 <115973449+109213054@users.noreply.github.com>
2023-09-29 17:51:55 +0800

    Update event-list.js

--- 前端/component/event-list.js
+++ 前端/component/event-list.js
@@ -1,10 +1,7 @@
-import { Text,View,FlatList, RefreshControl } from "react-native"
+import {View,FlatList, RefreshControl } from "react-native"
 import EventItem from "./event-item"
-import { useRoute } from "@react-navigation/native";
 export default EventList =({data}) => {
-  //console.log("我是Eventlist，收到資料為data:",data);
   const datalist = data;
-  //console.log("我是Eventlist，收到資料為datalist:",datalist);
     const renderItem =({item,index}) =>{
         return <EventItem rID={item} 
                           rName={datalist.rName[index]} 
@@ -12,7 +9,7 @@ export default EventList =({data}) => {
                           rPhone={datalist.rPhone[index]}
                           rAddress={datalist.rAddress[index]}
                           open={datalist.open[index]}
-                          opentwo={datalist.opentwo[index]}
+                          collect={datalist.collect[index]}
                           distance={datalist.distance[index]}/>
     }
     return (
