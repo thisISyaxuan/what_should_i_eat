a1731992537a2ca98dd8201f27c241736aeaff58
109213054 <zirou112233@gmail.com>
2023-11-04 14:47:05 +0800

    瀏覽紀錄點餐廳

--- 前端/routes/MyaccStack.js
+++ 前端/routes/MyaccStack.js
@@ -2,12 +2,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from "@react-navigation/native";
 import Myacc from '../screens/Myacc/Myacc';
-import EventDetailScreen from '../screens/Home/res-detail-screen';
 import 'react-native-gesture-handler';
-
+import ResInfo from '../screens/Home/res-detail-screen';
+import Errorfb from '../screens/Home/ErrorfeedBack';
 import Mycollect from '../screens/Myacc/Mycollect';
 import Login from '../screens/LoginRegister/Login';
 import MyHistory from '../screens/Myacc/History';
+import AddCashRes from '../screens/Add/AddCashRes';
 
 const Stack = createStackNavigator();
 const MyaccStack = () => (
@@ -50,6 +51,46 @@ const MyaccStack = () => (
                         },
                         headerTintColor: 'black'
                         }} />
+        <Stack.Screen name="錯誤回報" component={Errorfb} 
+                      options={{
+                        headerShown: true,
+                        headerStyle: {
+                            backgroundColor: '#f6d58a',
+                        },
+                        headerBackTitle: ' ',
+                        headerBackTitleStyle: {
+                            color: 'black', 
+                            fontSize: 16,
+                        },
+                        headerTintColor: 'black'
+                        }} />
+        <Stack.Screen name="造訪餐廳" component={AddCashRes} 
+                      options={{
+                        headerShown: true,
+                        headerStyle: {
+                            backgroundColor: '#f6d58a',
+                        },
+                        headerBackTitle: ' ',
+                        headerBackTitleStyle: {
+                            color: 'black', 
+                            fontSize: 16,
+                        },
+                        headerTintColor: 'black'
+                        }} />
+        <Stack.Screen name="餐廳資訊" component={ResInfo} 
+                      options={{
+                        headerShown: true,
+                        headerStyle: {
+                            backgroundColor: '#f6d58a',
+                        },
+                        headerBackTitle: ' ',
+                        headerBackTitleStyle: {
+                            color: 'black', 
+                            fontSize: 16,
+                        },
+                        headerTintColor: 'black'
+                        }} />
+
     </Stack.Navigator>
   </NavigationContainer>
 );
