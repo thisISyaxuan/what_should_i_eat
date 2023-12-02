8442471605b9eb67dec5b8f04bd75705592203d9
YAXUAN\ooooo <ooooo.yaxuan@gmail.com>
2023-12-03 03:12:41 +0800

    QA done

--- 前端/screens/Myacc/QA.js
+++ 前端/screens/Myacc/QA.js
@@ -10,40 +10,37 @@ const QA = () => {
         <ScrollView >
         <View style={styles.card}>
         <Text style={styles.title}>1. 帳號設定與登入</Text>
-        <Text style={styles.question}>Q: 如何創建新帳號？</Text>
-        <Text style={styles.answer}>A: 點選APP首頁右上角的「註冊」按鈕，填寫必要資訊即可成功創建帳號。</Text>
-        <Text style={styles.question}>Q: 忘記密碼怎麼辦？</Text>
-        <Text style={styles.answer}>A: 在登入頁面點選「忘記密碼」，輸入註冊時使用的電子郵件地址，系統將寄送密碼重設郵件至您的信箱。</Text>
-        <Text style={styles.question}>Q: 是否支援第三方帳號登入？</Text>
-        <Text style={styles.answer}>A: 目前我們提供Google和Facebook帳號登入選項，方便您快速進入APP。</Text>
-        
+        <Text style={styles.question}>Q：我可以註冊兩個帳號嗎？</Text>
+        <Text style={styles.answer}>A：為了確保每位使用者的帳號正常運作，我們限制每位使用者的帳號名稱與電子郵件必須不相同。所以若您想要註冊一個以上的帳號，請在上述提及的欄位輸入不同資訊。</Text>
+        <Text style={styles.answer}></Text>
         <Text style={styles.title}>2. 系統推薦餐廳</Text>
-        <Text style={styles.question}>Q: 如何使用系統推薦功能？</Text>
-        <Text style={styles.answer}>A: 在首頁或餐廳推薦頁面，點選您感興趣的餐廳名稱，即可查看餐廳資訊。利用搜尋功能，可以根據營業狀態、評分高低等條件進行排序。</Text>
-        <Text style={styles.question}>Q: 如何將餐廳加入收藏清單？</Text>
-        <Text style={styles.answer}>A: 在餐廳資訊頁面點選「加入收藏」，即可將該餐廳加入您的收藏清單中。</Text>
-        
+        <Text style={styles.question}>Q：登入後為什麼要等這麼久才有畫面？</Text>
+        <Text style={styles.answer}>A：系統在登入且尋求您的同意之後，系統會抓取你的定位，所以需要稍等約 10 秒中的時間 ~ </Text>
+        <Text style={styles.question}>Q：系統如何進行推薦機制？</Text>
+        <Text style={styles.answer}>A：本系統透過您註冊時所勾選之喜好項目、歷史記帳記錄以及其他使用者之歷史記帳記錄作為推薦的依據！若您想要更精準的系統推薦清單，那就多多記帳吧！</Text>
+        <Text style={styles.question}>Q：如何將餐廳加入收藏清單？</Text>
+        <Text style={styles.answer}>A：在點進餐廳資訊頁面之後，按下右上角的愛心，即可將該餐廳加入您的收藏清單！。</Text>
+        <Text style={styles.answer}></Text>
         <Text style={styles.title}>3. 記帳功能</Text>
-        <Text style={styles.question}>Q: 如何記錄餐飲花費？</Text>
-        <Text style={styles.answer}>A: 點選首頁的「記帳」功能，輸入消費金額、選擇用餐日期等資訊，點擊確認即可完成記帳。</Text>
-        <Text style={styles.question}>Q: 如何查看月支出圖表？</Text>
-        <Text style={styles.answer}>A: 在記帳功能中，點選「月支出圖表」，您可以清楚地看到每月的餐飲花費情形。</Text>
-        <Text style={styles.question}>Q: 能否查詢歷史記帳資訊？</Text>
-        <Text style={styles.answer}>A: 是的，點選「歷史記帳」，您可以查看以往的記帳紀錄，方便追蹤消費情況。</Text>
-        
-        
-        <Text style={styles.title}>4. 吉祥物蒐集</Text>
-        <Text style={styles.question}>Q: 如何簽到賺取遊戲幣？</Text>
-        <Text style={styles.answer}>A: 在首頁點選「吉祥物蒐集」，點擊簽到按鈕即可獲得遊戲幣獎勵。每日簽到更有額外獎勵。</Text>
-        <Text style={styles.question}>Q: 如何購買吉祥物？</Text>
-        <Text style={styles.answer}>A: 在吉祥物蒐集頁面，點選您喜歡的吉祥物，選擇購買即可使用遊戲幣取得。</Text>
-        
-        <Text style={styles.title}>5. 收藏清單</Text>
-        <Text style={styles.question}>Q: 如何查看收藏清單？</Text>
-        <Text style={styles.answer}>A: 在首頁點選「收藏清單」，您可以查看已收藏的餐廳清單，隨時找回您感興趣的餐廳。</Text>
-        <Text style={styles.question}>Q: 是否能分享收藏清單？</Text>
-        <Text style={styles.answer}>A: 目前我們提供分享功能，您可以將收藏清單分享至社交平台或通訊應用，與好友交流美食心得。</Text>
-        
+        <Text style={styles.question}>Q：如何記錄餐飲花費？</Text>
+        <Text style={styles.answer}>A：系統提供兩個方式能夠記帳！第一是在餐廳資訊頁面中的「造訪餐廳」，系統會將餐廳名稱自動填入；第二是「記一筆」功能，輸入指定欄位資訊即可完成記帳。</Text>
+        <Text style={styles.question}>Q：如何查看月支出圖表？</Text>
+        <Text style={styles.answer}>A：在「我的錢包」頁面中，點選右上角的圓餅圖圖示，即可清楚地看到每月的餐飲花費情形。</Text>
+        <Text style={styles.answer}></Text>
+        <Text style={styles.title}>4. 精靈與雞腿幣</Text>
+        <Text style={styles.question}>Q：精靈在系統中可以做什麼？</Text>
+        <Text style={styles.answer}>A：精靈主要提供兩種用途，一為使用者可蒐集、更改大頭貼與介面；二為點擊「我的精靈」頁面之下方精靈，系統將會隨機抽選一間餐廳，提供建議給猶豫不決的使用者們。</Text>
+        <Text style={styles.question}>Q：如何簽到賺取雞腿幣？</Text>
+        <Text style={styles.answer}>A：方法有二，一是在「我的精靈」頁面，每日提供一次簽到機會，可賺取 100 雞腿幣；二是透過記帳，每成功記帳一筆記錄，也可賺賺取 20 雞腿幣。</Text>
+        <Text style={styles.question}>Q：如何購買精靈與更換大頭貼？</Text>
+        <Text style={styles.answer}>A：在「我的精靈」頁面中點選精靈圖鑑，即可看見所有系統精靈，尚未擁有的精靈可透過雞腿幣購買，已經擁有的精靈可選擇成為帳號大頭貼。</Text>
+        <Text style={styles.answer}></Text>
+        <Text style={styles.title}>5. 其他</Text>
+        <Text style={styles.question}>Q：隨機抽選一間餐廳之後，要去哪裡可以再看到剛剛那間的餐廳資訊？</Text>
+        <Text style={styles.answer}>A：在帳號設定中可點選「瀏覽紀錄」，便可以依序找到您瀏覽過的資聽，隨時找回您感興趣的餐廳。</Text>
+        <Text style={styles.question}>Q：系統資訊什麼時候會更新？</Text>
+        <Text style={styles.answer}>A：為了提供使用者最完整的服務，我們將定期維護系統資訊。若使用者仍然發現系統資訊有誤，歡迎在餐廳資訊頁面點選「錯誤回報」，提供系統最新資訊，我們將會在審核過後，更新系統資訊，並獎勵使用者 200 雞腿幣。</Text>
+
             </View>
         </ScrollView>
   );
@@ -51,8 +48,8 @@ const QA = () => {
 
 const styles = StyleSheet.create({
   card:{
-    flex: 1,
-    marginLeft: 20,
+    flex:1,
+    marginLeft:20,
     marginRight:20,
     padding:10,
     marginTop:5,
