0a1a24c3b75ad7a05a744f297efda7c281e165cb
109213054 <zirou112233@gmail.com>
2023-12-04 17:42:02 +0800

    前端記帳刪除!

--- 前端/screens/Wallet/Wallet.js
+++ 前端/screens/Wallet/Wallet.js
@@ -5,6 +5,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FlatList } from 'react-native-gesture-handler';
 import { link } from '../../data/apiLink';
 import { color } from 'react-native-elements/dist/helpers';
+import { EvilIcons } from '@expo/vector-icons'
+import { Alert } from 'react-native';
 
 export default function Wallet() {
   const currentDate = new Date();
@@ -12,22 +14,70 @@ export default function Wallet() {
   const [selected, setSelected] = useState(formattedDate);//一開始為當下時間
   const [dateData, setDateData] = useState([]);//依日期對應的記帳資訊
   const [total, setTotal] = useState(0);//總金額
+  const [isdelete,setisdelete] = useState(true);
   const initial = {"data": [ {"cid": 1, "price": 70, "rating": 4.1, "ResName": "蘇嬤嬤湯圓", "which_meal": 3,"my_text":"好累=..= "},
                              {"cid": 2, "price": 80, "rating": 5, "ResName": "大仁鍋貼", "which_meal": 1}, 
-                             {"cid": 3, "price": 90, "rating": 4.5, "ResName": "麥當勞", "which_meal": 2},
-                             {"cid": 4, "price": 90, "rating": 4.5, "ResName": "麥當勞", "which_meal": 1},
-                             {"cid": 5, "price": 90, "rating": 4.5, "ResName": "麥當勞", "which_meal": 2},
-                             {"cid": 6, "price": 90, "rating": 4.5, "ResName": "麥當勞", "which_meal": 0},
-                             {"cid": 7, "price": 90, "rating": 4.5, "ResName": "麥當勞", "which_meal": 2} ], 
+                             {"cid": 3, "price": 90, "rating": 4.5, "ResName": "麥三", "which_meal": 2},
+                             {"cid": 8, "price": 90, "rating": 4.5, "ResName": "麥四", "which_meal": 1},
+                             {"cid": 5, "price": 90, "rating": 4.5, "ResName": "麥五", "which_meal": 2},
+                             {"cid": 6, "price": 90, "rating": 4.5, "ResName": "麥六", "which_meal": 0},
+                             {"cid": 7, "price": 90, "rating": 4.5, "ResName": "麥七", "which_meal": 2} ], 
                             "total": {"total": 0}}
 
   useEffect(() => {
     fetchDateData(selected); // 初始化時取得當天數據
   }, []);
+  
+
+
+
+  const handleDelete = (cid) => {
+    // 顯示確認刪除的提示框
+    Alert.alert( "確認刪除", "你確定要刪除這筆資料嗎？",
+      [
+        { text: "取消", style: "cancel" },
+        { text: "確定",
+          onPress: async () => {
+            try {
+              //console.log(cid)
+              //傳給後端資料
+              const updatedData = dateData.filter(item => item.cid !== cid);
+              //console.log("按下的數字為:",updatedData)
+              const userToken = await AsyncStorage.getItem('userToken');
+              if (userToken) {
+                const response = await fetch(link.deletecash, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Token ${userToken}`,
+                  },
+                });
+                if (response.ok) {
+                  await updateDataSomehow(updatedData);
+                }else{
+                  Alert.alert("刪除失敗，請再試一次!")
+                }
+              }
+            } catch (error) {
+              console.error('Error deleting data:', error);
+            }
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const updateDataSomehow = async (updatedData) => {
+    // 在這裡實現實際的更新操作，可以是 API 請求或者是本地狀態更新
+    // 這裡只是一個示例，實際情況需要根據你的應用邏輯進行修改
+    setDateData(updatedData);
+  };
+
 
   const renderItem = ({ item }) => (
     <View style={styles.costdetail}>
-      <View style={[{borderBottomWidth:1,flexDirection:'row',borderBottomColor:"#C0C0C0"}]}>
+      <View style={[{flexDirection:'row',alignItems:'center'}]}>
       <View style={[styles.circle,{backgroundColor: item.which_meal === 0 ? '#415CA4'
                                                   : item.which_meal === 1 ? '#FFB755'
                                                   : item.which_meal === 2 ? '#338168'
@@ -44,14 +94,20 @@ export default function Wallet() {
       <View style={styles.money}>
         <Text style={styles.textMoney}>$ {item.price}</Text>
       </View>
+      <View style={{flexDirection:'row'}}>
+      {isdelete ? (
+      <TouchableOpacity style={{margin:10,width:30,justifyContent:'center',alignItems:'center',flexDirection:'row'}}
+                        onPress={() => handleDelete(item.cid)}>
+        <EvilIcons name="trash" size={30} color="red" />
+      </TouchableOpacity>
+      ) : (null)
+      }
 
-      <View style={{margin:10,width:60,justifyContent:'center',alignItems:'center',backgroundColor:'red',borderRadius:100}}>
-        <Text style={{color:'white',fontWeight:'bold'}}>-</Text>
       </View>
-      
       </View>
     </View>
   );
+
 
   const fetchDateData = async (date) => {
     try {
@@ -99,8 +155,15 @@ export default function Wallet() {
       <View>
       <View style={{flexDirection:'row',justifyContent:'space-between',marginTop:50}}>
       <Text style={[{fontSize:16},{margin:5}]}>今日總支出：${total}</Text>
-      <TouchableOpacity style={{padding:5,marginRight:5}}>
-        <Text style={{fontSize:16,color:'red'}}>編輯</Text></TouchableOpacity>
+      
+        <TouchableOpacity style={{padding:5,marginRight:5}} onPress={() => setisdelete(prevState => !prevState)}>
+          {isdelete ? (
+              <Text style={{fontSize:16,color:'black'}}>取消編輯</Text>
+            ) : (
+              <Text style={{fontSize:16,color:'red'}}>編輯</Text>
+            ) 
+        }
+        </TouchableOpacity>
       </View>
       <View style={{height:240}}>
       <FlatList
@@ -134,15 +197,18 @@ const styles = StyleSheet.create({
     backgroundColor:'white',
     //flexDirection:'row',
     padding:10,
-    
+    alignContent:'center',
+    borderBottomWidth:1,
+    borderBottomColor:"#C0C0C0"
   },
   circle:{
     width:'16%',
     height:40,
     alignContent:'center',
     justifyContent:'center',
+    alignItems:'center',
     borderRadius:50,
-    padding:10,
+    padding:5,
   },
   square:{
     backgroundColor:'white',
